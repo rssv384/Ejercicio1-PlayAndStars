37c153fdfa95a8c4feb0112cf53f106df4150171
Raúl Soto <rssotovejar@gmail.com>
2023-09-05 08:46:40 -0700

    Ej 2: Sección contacto

--- js/main.js
+++ js/main.js
@@ -22,3 +22,25 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 });
+
+// Formulario de contacto (Funcionalidad temporal)
+const contactoForm = document.getElementById('contact-form');
+
+contactoForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const nombre = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const mensaje = document.getElementById('message').value;
+
+	alert(
+		'Nuevo Mensaje - Nombre: ' +
+			nombre +
+			'- Email: ' +
+			email +
+			'- Mensaje: ' +
+			mensaje
+	);
+
+	contactoForm.reset();
+});
